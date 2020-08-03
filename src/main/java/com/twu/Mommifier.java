b14b2dd57dc729424c1d6d63eeca4895f1dc6f82
ghjhh <798389920@qq.com>
2020-08-03 19:57:52 +0800

    [WangZuowen]feat:Pass test should_throw_exception_when_input_null_or_empty.

--- src/main/java/com/twu/Mommifier.java
+++ src/main/java/com/twu/Mommifier.java
@@ -7,16 +7,19 @@ package com.twu;
  */
 public class Mommifier {
 
-    public String momifierString(String inputString) {
+    public String momifierString(String inputString) throws Exception {
+        if (inputString == null || "".equals(inputString)) {
+            throw new Exception("input is wrong");
+        }
         int length = inputString.length();
         double threshold = 0.3;
         int vowelsLength = 0;
         for (int i = 0; i < length; i++) {
-            if(isVowels(inputString.charAt(i))){
+            if (isVowels(inputString.charAt(i))) {
                 vowelsLength++;
             }
         }
-        if(vowelsLength > threshold * length){
+        if (vowelsLength > threshold * length) {
             inputString = addMommyInString(inputString);
         }
         return inputString;
@@ -24,17 +27,17 @@ public class Mommifier {
 
     private String addMommyInString(String inputString) {
         String resultString = inputString;
-        for (int i = 0; i < inputString.length() -1; i++) {
-            if(isVowels(inputString.charAt(i)) && isVowels(inputString.charAt(i+1))){
-                String preString = resultString.substring(0,i+1);
-                String nextString = resultString.substring(i+1,inputString.length());
+        for (int i = 0; i < inputString.length() - 1; i++) {
+            if (isVowels(inputString.charAt(i)) && isVowels(inputString.charAt(i + 1))) {
+                String preString = resultString.substring(0, i + 1);
+                String nextString = resultString.substring(i + 1, inputString.length());
                 resultString = preString + "mommy" + nextString;
             }
         }
         return resultString;
     }
 
-    private boolean isVowels(char c){
+    private boolean isVowels(char c) {
         String vowels = "aeiou";
         return vowels.contains(String.valueOf(c));
     }
--- src/test/java/com/twu/MommifierTest.java
+++ src/test/java/com/twu/MommifierTest.java
@@ -1,37 +1,41 @@
 package com.twu;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MommifierTest {
     @Test
-    void should_do_nothing_when_vowels_less_than_30_percents() {
+    void should_do_nothing_when_vowels_less_than_30_percents() throws Exception {
         String vowelsLessThan30 = "hello";
         String vowelsLessThan30WithContinuousVowels = "gosss";
         Mommifier mommifier = new Mommifier();
-        assertEquals(vowelsLessThan30,mommifier.momifierString(vowelsLessThan30));
-        assertEquals(vowelsLessThan30WithContinuousVowels,mommifier.momifierString(vowelsLessThan30WithContinuousVowels));
+        assertEquals(vowelsLessThan30, mommifier.momifierString(vowelsLessThan30));
+        assertEquals(vowelsLessThan30WithContinuousVowels, mommifier.momifierString(vowelsLessThan30WithContinuousVowels));
     }
+
     @Test
-    void should_do_nothing_when_vowels_more_than_30_percents_but_no_continuous_vowels(){
+    void should_do_nothing_when_vowels_more_than_30_percents_but_no_continuous_vowels() throws Exception {
         String vowelsMoreThan30WithoutContinuousVowels = "gose";
         Mommifier mommifier = new Mommifier();
-        assertEquals(vowelsMoreThan30WithoutContinuousVowels,mommifier.momifierString(vowelsMoreThan30WithoutContinuousVowels));
+        assertEquals(vowelsMoreThan30WithoutContinuousVowels, mommifier.momifierString(vowelsMoreThan30WithoutContinuousVowels));
     }
+
     @Test
-    void should_insert_mommy_between_continuous_vowels_when_vowels_more_than_30_percents(){
+    void should_insert_mommy_between_continuous_vowels_when_vowels_more_than_30_percents() throws Exception {
         String vowelsMoreThan30WithContinuousVowels = "goes";
         Mommifier mommifier = new Mommifier();
         String exceptString = "gomommyes";
-        assertEquals(exceptString,mommifier.momifierString(vowelsMoreThan30WithContinuousVowels));
+        assertEquals(exceptString, mommifier.momifierString(vowelsMoreThan30WithContinuousVowels));
     }
+
     @Test
-    void should_throw_exception_when_input_null_or_empty(){
+    void should_throw_exception_when_input_null_or_empty() {
         Mommifier mommifier = new Mommifier();
-        Throwable exceptionInputEmpty = assertThrows(Exception.class,()->mommifier.momifierString(""));
-        Throwable exceptionInputNull = assertThrows(Exception.class,()->mommifier.momifierString(null));
-        assertEquals("input is wrong",exceptionInputEmpty.getMessage());
-        assertEquals("input is wrong",exceptionInputNull.getMessage());
+        Throwable exceptionInputEmpty = assertThrows(Exception.class, () -> mommifier.momifierString(""));
+        Throwable exceptionInputNull = assertThrows(Exception.class, () -> mommifier.momifierString(null));
+        assertEquals("input is wrong", exceptionInputEmpty.getMessage());
+        assertEquals("input is wrong", exceptionInputNull.getMessage());
     }
 }
